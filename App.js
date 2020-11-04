8358c9b8865346fd204f2c0751c574b72137cf2b
thatmiphilit <phihvm18@gmail.com>
2020-11-05 01:24:42 +0700

    add tab, screen and UX: 100 bruh

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
 import React from 'react'
 
 //Các màn hình render
@@ -16,22 +18,76 @@ import More from './screen/More'
 import AddStaff from './screen/AddStaff'
 import Staff from './screen/Staff'
 
-const Stack = createStackNavigator() //điều hướng 
+const Tab = createMaterialBottomTabNavigator()
+
+const MainMenuStack = createStackNavigator()
+const CartStack = createStackNavigator()
+const MoreStack = createStackNavigator()
+const StaffStack = createStackNavigator()
+
+function MainMenuStackScreen() {
+  return(
+    <MainMenuStack.Navigator>
+      <MainMenuStack.Screen name="Home" component={MainMenu} options={{title: 'Home', headerTitleStyle: {alignSelf:'center'}}}/>
+      <MainMenuStack.Screen name="Food Menu" component={Categories} options={{title: 'Menu'}}/>
+      <MainMenuStack.Screen name="Fast Foods Details" component={FastFoodsDetails} options={{title: 'Thức ăn nhanh'}}/>
+      <MainMenuStack.Screen name="Hot Pot Details" component={HotPotDetails} options={{title: 'Lẩu'}}/>
+      <MainMenuStack.Screen name="Fried Details" component={FriedDetails} options={{title: 'Món chiên'}}/> 
+      <MainMenuStack.Screen name="Drinks Details" component={DrinksDetails} options={{title: 'Thức uống'}}/>
+      <MainMenuStack.Screen name="Desserts Details" component={DessertsDetails} options={{title: 'Tráng miệng'}}/>
+      <MainMenuStack.Screen name="Staff" component={Staff} options={{title: 'Nhân sự'}}/>
+      <MainMenuStack.Screen name="Add Staff" component={AddStaff} options={{title: 'Thêm nhân viên'}}/>
+    </MainMenuStack.Navigator>
+  )
+}
+
+function CartStackScreen() {
+  return(
+    <CartStack.Navigator>
+      <CartStack.Screen name="Cart" component={Cart} options={{title: 'Đơn hàng', headerTitleStyle: {alignSelf:'center'}}}/>
+    </CartStack.Navigator>
+  )
+}
+
+function StaffStackScreen() {
+  return(
+    <StaffStack.Navigator>
+      <StaffStack.Screen name="Staff" component={Cart} options={{title: 'Nhân sự', headerTitleStyle: {alignSelf:'center'}}}/>
+    </StaffStack.Navigator>
+  )
+}
+
+function MoreStackScreen() {
+  return(
+    <MoreStack.Navigator>
+      <MoreStack.Screen name="More" component={More} options={{title: 'Mở rộng', headerTitleStyle: {alignSelf:'center'}}}/>
+    </MoreStack.Navigator>
+  )
+}
 
 export default function App() {
     return (
       <NavigationContainer>
-          <Stack.Navigator initialRouteName="Main menu">
-            <Stack.Screen name="Main Menu" component={MainMenu} options={{title: 'Home', headerTitleStyle: {alignSelf:'center'}}}/>
-            <Stack.Screen name="Food Menu" component={Categories} options={{title: 'Menu'}}/>
-            <Stack.Screen name="Fast Foods Details" component={FastFoodsDetails} options={{title: 'Thức ăn nhanh'}}/>
-            <Stack.Screen name="Hot Pot Details" component={HotPotDetails} options={{title: 'Lẩu'}}/>
-            <Stack.Screen name="Fried Details" component={FriedDetails} options={{title: 'Món chiên'}}/> 
-            <Stack.Screen name="Drinks Details" component={DrinksDetails} options={{title: 'Thức uống'}}/>
-            <Stack.Screen name="Desserts Details" component={DessertsDetails} options={{title: 'Tráng miệng'}}/>
-            <Stack.Screen name="Staff" component={Staff} options={{title: 'Nhân sự'}}/>
-            <Stack.Screen name="Add Staff" component={AddStaff} options={{title: 'Thêm nhân viên'}}/>
-          </Stack.Navigator>
+          <Tab.Navigator initialRouteName="Home"
+                         barStyle={{ backgroundColor: '#de5543' }}
+                         >
+            <Tab.Screen name="Home" component={MainMenuStackScreen} options={{tabBarLabel: 'Home',
+                                    tabBarIcon: ({ color }) => (
+                                      <MaterialCommunityIcons name="home-outline" color={color} size={26} />
+                                    )}} />
+            <Tab.Screen name="Cart" component={CartStackScreen} options={{tabBarLabel: 'Đơn hàng',
+                                    tabBarIcon: ({ color }) => (
+                                      <MaterialCommunityIcons name="cart-outline" color={color} size={26} />
+                                    )}} />
+            <Tab.Screen name="Staff" component={StaffStackScreen} options={{tabBarLabel: 'Nhân sự',
+                                    tabBarIcon: ({ color }) => (
+                                      <MaterialCommunityIcons name="account-outline" color={color} size={26} />
+                                    )}} />
+            <Tab.Screen name="More" component={MoreStackScreen} options={{tabBarLabel: 'Mở rộng',
+                                    tabBarIcon: ({ color }) => (
+                                      <MaterialCommunityIcons name="layers-outline" color={color} size={26} />
+                                    )}} />
+          </Tab.Navigator>
       </NavigationContainer>
   )
 }
