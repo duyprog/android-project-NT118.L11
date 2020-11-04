0de6e579d21ebeeff9fa48afee622f9523e63ba6
thatmiphilit <phihvm18@gmail.com>
2020-11-04 21:58:22 +0700

    add more screen

--- screen/MainMenu.js
+++ screen/MainMenu.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
-import MainMenuList from '../components/MainMenuListItems'
 
 import Table from '../image/mainMenu/dining-table.png'
 import TakeAway from '../image/mainMenu/take-away.png'
@@ -9,24 +8,40 @@ import CategoryListItems from '../components/CategoryListItems'
 
 const MainMenu = ({ navigation }) => {
     return (
-        <TouchableOpacity
-            activeOpacity={0.5}
-            onPress={() => {
+        <View>
+            <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                    navigation.navigate('Food Menu')
+                }}>
+                <CategoryListItems title={"Tại chỗ"} uri={require('../image/mainMenu/dining-table.png')} />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
                 navigation.navigate('Food Menu')
-            }}>
-            <CategoryListItems title={"Tại chỗ"} uri={require('../image/mainMenu/dining-table.png')} />
-        </TouchableOpacity>   
+                }}>
+                <CategoryListItems title={"Mang đi"} uri={require('../image/mainMenu/take-away.png')} />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                navigation.navigate('Food Menu')
+                }}>
+                <CategoryListItems title={"Hóa đơn"} uri={require('../image/mainMenu/bill.png')} />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                navigation.navigate('Staff')
+                }}>
+                <CategoryListItems title={"Nhân sự"} uri={require('../image/mainMenu/staff.png')} />
+            </TouchableOpacity>
+        </View>
     )
-
 }
-
-const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 8},
-    wrapper:{
-        flex: 1,
-        paddingHorizontal: 8
-    }
-})
 
 export default MainMenu
