86ae8f6db83eb7b2aa89298b635801390f594de9
Duy Prog <18520669@gm.uit.edu.vn>
2020-11-04 14:45:41 +0700

    delete import category.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import CategoryListItems from './components/CategoryListItems';
-import CategoryList from './components/CategoryList';
 import FoodItems from './items/FoodItems'
 import FoodDetails from './screen/FoodDetails'
 AppRegistry.registerComponent(appName, () => App);
