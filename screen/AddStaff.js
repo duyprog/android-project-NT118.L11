bdc8198853b5bc3386c96bed21e9f32db7c1426c
Duy Prog <18520669@gm.uit.edu.vn>
2020-11-05 00:53:58 +0700

    Them chuc nang add nhan vien

--- screen/AddStaff.js
+++ screen/AddStaff.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react'
 import { Button, Text, TextInput, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-
+import {insertAStaff} from '../networking/server'
 export default class AddStaff extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            identify:'',
+            staffName: '',
+            position: '',
+        }
+    }
+
     render() {
         return (
             <View>
                 <View>
                     <Text> Họ tên </Text>
-                    <TextInput placeholder="Họ tên" />
+                    <TextInput 
+                        onChangeText= { text => this.setState({staffName: text})}
+                        placeholder="Họ tên" />
                 </View>
 
                 <View>
                     <Text> ID </Text>
-                    <TextInput placeholder="ID" />
+                    <TextInput
+                        onChangeText={text => this.setState({identify: text})}
+                        placeholder="ID" />
                 </View>
 
                 <View>
                     <Text> Chức vụ </Text>
-                    <TextInput placeholder="Chức vụ" />
+                    <TextInput
+                        onChangeText={text => this.setState({position: text})}   
+                        placeholder="Chức vụ" />
                 </View>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() =>{
+                    if (this.state.staffName.length == 0|| this.state.identify.length == 0){
+                        alert("Bạn phải nhập đầy đủ tên và id");
+                        return;
+                    }
+                    const newStaff = {
+                        identify: this.state.identify,
+                        staffName: this.state.staffName,
+                        position: this.state.position
+                    }
+                    insertAStaff(newStaff).then((res) =>{
+                            alert('Them thanh cong');
+                    })
+                }}>
                     <Text> Thêm </Text>
                 </TouchableOpacity>
             </View>
