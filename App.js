1337efb748599b30a46829f0c1ff2422f45ee03f
thatmiphilit <phihvm18@gmail.com>
2020-11-04 16:10:35 +0700

    rename item

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Categories from './screen/Categories';
 import FastFoodsDetails from './screen/FastFoodsDetails';
 import DrinksDetails from './screen/DrinksDetails';
 import DessertsDetails from './screen/DessertsDetails';
+import HotPotDetails from './screen/HotPotDetails'
+import FriedDetails from './screen/FriedDetails'
 import React from 'react'
 
 const Stack = createStackNavigator();
@@ -13,9 +15,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Category">
         <Stack.Screen name="Main Menu" component={Categories} options={{title: 'Menu', headerTitleStyle: {alignSelf:'center'}}}/>
-        <Stack.Screen name="Fast Foods Details" component={FastFoodsDetails} options={{title: 'Fast Foods'}}/>
-        <Stack.Screen name="Drinks Details" component={DrinksDetails} options={{title: 'Drinks'}}/>
-        <Stack.Screen name="Desserts Details" component={DessertsDetails} options={{title: 'Desserts'}}/> 
+        <Stack.Screen name="Fast Foods Details" component={FastFoodsDetails} options={{title: 'Thức ăn nhanh'}}/>
+        <Stack.Screen name="Hot Pot Details" component={HotPotDetails} options={{title: 'Lẩu'}}/>
+        <Stack.Screen name="Fried Details" component={FriedDetails} options={{title: 'Món chiên'}}/> 
+        <Stack.Screen name="Drinks Details" component={DrinksDetails} options={{title: 'Thức uống'}}/>
+        <Stack.Screen name="Desserts Details" component={DessertsDetails} options={{title: 'Tráng miệng'}}/> 
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screen/Categories.js
+++ screen/Categories.js
@@ -3,30 +3,47 @@ import { Text, View, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
 import CategoryListItems from '../components/CategoryListItems'
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Categories = ({navigation}) => {
         return (
             
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <TouchableOpacity 
                     activeOpacity={0.5}
                     onPress={()=>{
-                        navigation.navigate('Fast Food Details')
+                        navigation.navigate('Fast Foods Details')
                     }}
-                ><CategoryListItems title={"Fast Foods"} uri={require('../image/fast-food.png')}/></TouchableOpacity>
+                ><CategoryListItems title={"Thức ăn nhanh"} uri={require('../image/fast-food.png')}/></TouchableOpacity>
+
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    onPress={() =>{
+                        navigation.navigate('Hot Pot Details');
+                    }}
+                ><CategoryListItems title={"Lẩu"} uri={require('../image/hot-pot.png')}/></TouchableOpacity>
+
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    onPress={() =>{
+                        navigation.navigate('Fried Details');
+                    }}
+                ><CategoryListItems title={"Món chiên"} uri={require('../image/fried.png')}/></TouchableOpacity>
+
                 <TouchableOpacity
                     activeOpacity={0.5}
                     onPress={() =>{
                         navigation.navigate('Drinks Details');
                     }}
-                ><CategoryListItems title={"Drinks"} uri={require('../image/coke.png')}/></TouchableOpacity>
+                ><CategoryListItems title={"Thức uống"} uri={require('../image/coke.png')}/></TouchableOpacity>
+
                 <TouchableOpacity
                     activeOpacity={0.5}
                     onPress={() =>{
                         navigation.navigate('Desserts Details');
                     }}
-                ><CategoryListItems title={"Desserts"} uri={require('../image/dessert.png')}/></TouchableOpacity>
-            </View>
+                ><CategoryListItems title={"Tráng miệng"} uri={require('../image/dessert.png')}/></TouchableOpacity>
+            </ScrollView>
         )
 }
 
@@ -34,8 +51,6 @@ const styles = StyleSheet.create({
     container: {
         marginTop: 10,
         flex: 1, 
-        alignItems: 'stretch', 
-        justifyContent: 'center',
         paddingLeft: 16, 
         paddingRight: 16,
 
