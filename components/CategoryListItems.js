1abd41f94d9d02ef164cc3643c0afc32092dcac7
thatmiphilit <phihvm18@gmail.com>
2020-11-04 16:10:35 +0700

    rename item

--- components/CategoryListItems.js
+++ components/CategoryListItems.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     containter:{
         alignItems: 'center',
         padding: 16,
-        borderRadius: 8,
+        borderRadius: 12,
         backgroundColor: '#FFF',
         elevation: 8,
         marginBottom: 16
