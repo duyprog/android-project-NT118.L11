d365bbf57d97ddb0fa860d49136e895e4ad400e8
thatmiphilit <phihvm18@gmail.com>
2020-11-05 20:46:22 +0700

    add login screen

--- screen/Categories.js
+++ screen/Categories.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity} from 'react-native'
+import React from 'react'
+import { StyleSheet, TouchableOpacity} from 'react-native'
 import CategoryListItems from '../components/CategoryListItems'
 import { ScrollView } from 'react-native-gesture-handler';
 
 const Categories = ({navigation}) => {
         return (
-            
-            <ScrollView style={styles.container}>
-                <TouchableOpacity 
-                    activeOpacity={0.5}
-                    onPress={()=>{
-                        navigation.navigate('Fast Foods Details')
-                    }}
-                ><CategoryListItems title={"Thức ăn nhanh"} uri={require('../image/fast-food.png')}/></TouchableOpacity>
+            <View>
+                <ScrollView style={styles.container}>
+                    <TouchableOpacity 
+                        activeOpacity={0.5}
+                        onPress={()=>{
+                            navigation.navigate('Fast Foods Details')
+                        }}
+                    ><CategoryListItems title={"Thức ăn nhanh"} uri={require('../image/fast-food.png')}/></TouchableOpacity>
 
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>{
-                        navigation.navigate('Hot Pot Details');
-                    }}
-                ><CategoryListItems title={"Lẩu"} uri={require('../image/hot-pot.png')}/></TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        onPress={() =>{
+                            navigation.navigate('Hot Pot Details');
+                        }}
+                    ><CategoryListItems title={"Lẩu"} uri={require('../image/hot-pot.png')}/></TouchableOpacity>
 
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>{
-                        navigation.navigate('Fried Details');
-                    }}
-                ><CategoryListItems title={"Món chiên"} uri={require('../image/fried.png')}/></TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        onPress={() =>{
+                            navigation.navigate('Fried Details');
+                        }}
+                    ><CategoryListItems title={"Món chiên"} uri={require('../image/fried.png')}/></TouchableOpacity>
 
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>{
-                        navigation.navigate('Drinks Details');
-                    }}
-                ><CategoryListItems title={"Thức uống"} uri={require('../image/coke.png')}/></TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        onPress={() =>{
+                            navigation.navigate('Drinks Details');
+                        }}
+                    ><CategoryListItems title={"Thức uống"} uri={require('../image/coke.png')}/></TouchableOpacity>
 
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>{
-                        navigation.navigate('Desserts Details');
-                    }}
-                ><CategoryListItems title={"Tráng miệng"} uri={require('../image/dessert.png')}/></TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        onPress={() =>{
+                            navigation.navigate('Desserts Details');
+                        }}
+                    ><CategoryListItems title={"Tráng miệng"} uri={require('../image/dessert.png')}/></TouchableOpacity>
             </ScrollView>
+            </View>
         )
 }
 
