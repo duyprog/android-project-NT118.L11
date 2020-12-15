5bc6086c8addb4a91ae7b2a13017f007f467dd1c
Chung Tin <tinanthedrew@gmail.com>
2020-12-15 10:24:57 +0700

    Add Login screen, test API

--- App.js
+++ App.js
@@ -1,92 +1,78 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import React from 'react'
 
 //import Stack Screen
-import MainMenuStackScreen from './stackScreen/MainMenuStack';
-import StaffStackScreen from './stackScreen/StaffStack';
-import CartStackScreen from './stackScreen/CartStack';
-import MoreStackScreen from './stackScreen/MoreStack';
+import MainMenuStackScreen from './stackScreen/MainMenuStack'
+import StaffStackScreen from './stackScreen/StaffStack'
+import CartStackScreen from './stackScreen/CartStack'
+import MoreStackScreen from './stackScreen/MoreStack'
 
 //import Login Screen
-import LoginScreen from './screen/LoginScreen';
+import LoginScreen from './screen/LoginScreen'
 
 const AuthStack = createStackNavigator();
 const AuthStackScreen = () => (
-  <AuthStack.Navigator>
-    <AuthStackScreen.Screen name="Login" component={LoginScreen} />
-  </AuthStack.Navigator>
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+    </AuthStack.Navigator>
 );
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator()
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AuthStackScreen />
-      <Tab.Navigator
-        initialRouteName="Home"
-        activeColor="#fff"
-        barStyle={{backgroundColor: '#de5543'}}>
-        <Tab.Screen
-          name="Home"
-          component={MainMenuStackScreen}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons
-                name="home-outline"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={CartStackScreen}
-          options={{
-            tabBarLabel: 'Đơn hàng',
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons
-                name="cart-outline"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Staff"
-          component={StaffStackScreen}
-          options={{
-            tabBarLabel: 'Nhân sự',
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons
-                name="account-outline"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="More"
-          component={MoreStackScreen}
-          options={{
-            tabBarLabel: 'Mở rộng',
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons
-                name="layers-outline"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    return (
+      <NavigationContainer>
+        <AuthStackScreen />
+          {/* <Tab.Navigator  
+              initialRouteName="Home"
+              activeColor= "#fff"
+              barStyle={{ backgroundColor: '#de5543' }}>
+            <Tab.Screen 
+                name="Home" 
+                component={MainMenuStackScreen} 
+                options={{tabBarLabel: 'Home',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons 
+                        name="home-outline" 
+                        color={color} 
+                        size={26} />
+                  )}} />
+            <Tab.Screen 
+                name="Cart" 
+                component={CartStackScreen} 
+                options={{tabBarLabel: 'Đơn hàng',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons 
+                        name="cart-outline" 
+                        color={color} 
+                        size={26} />
+                  )}} />
+            <Tab.Screen 
+                name="Staff" 
+                component={StaffStackScreen} 
+                options={{tabBarLabel: 'Nhân sự',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons 
+                        name="account-outline" 
+                        color={color} 
+                        size={26} />
+                  )}} />
+            <Tab.Screen 
+                name="More" 
+                component={MoreStackScreen} 
+                options={{tabBarLabel: 'Mở rộng',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons 
+                        name="layers-outline" 
+                        color={color} 
+                        size={26} />
+                  )}} />
+          </Tab.Navigator> */}
+      </NavigationContainer>
+  )
 }
+
+
--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default class LoginScreen extends Component {
     state={
         email:"",
         password:""
       }
+
+      
 
     render() {
         return (
