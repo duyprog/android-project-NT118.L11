d200702ba17a196c977e70fcc058cfcd79936d38
Chung Tin <tinanthedrew@gmail.com>
2020-12-18 19:16:30 +0700

    Add Splash Screen. Change LoginScreen -> SignInScreen. Remember to remove Package_lock.json and Node_modules before Yarn Install all packages again for properly App loading.

--- App.js
+++ App.js
@@ -1,78 +1,121 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-//import Stack Screen
-import MainMenuStackScreen from './stackScreen/MainMenuStack'
-import StaffStackScreen from './stackScreen/StaffStack'
-import CartStackScreen from './stackScreen/CartStack'
-import MoreStackScreen from './stackScreen/MoreStack'
+import { Provider as PaperProvider } from 'react-native-paper';
 
-//import Login Screen
-import LoginScreen from './screen/LoginScreen'
+import RootStackScreen from './screen/RootStackScreen';
 
-const AuthStack = createStackNavigator();
-const AuthStackScreen = () => (
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="Login" component={LoginScreen} />
-    </AuthStack.Navigator>
-);
+import AsyncStorage from 'react-native';
 
-const Tab = createMaterialBottomTabNavigator()
+const AuthContext = React.createContext();
 
-export default function App() {
-    return (
-      <NavigationContainer>
-        <AuthStackScreen />
-          {/* <Tab.Navigator  
-              initialRouteName="Home"
-              activeColor= "#fff"
-              barStyle={{ backgroundColor: '#de5543' }}>
-            <Tab.Screen 
-                name="Home" 
-                component={MainMenuStackScreen} 
-                options={{tabBarLabel: 'Home',
-                  tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons 
-                        name="home-outline" 
-                        color={color} 
-                        size={26} />
-                  )}} />
-            <Tab.Screen 
-                name="Cart" 
-                component={CartStackScreen} 
-                options={{tabBarLabel: 'Đơn hàng',
-                  tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons 
-                        name="cart-outline" 
-                        color={color} 
-                        size={26} />
-                  )}} />
-            <Tab.Screen 
-                name="Staff" 
-                component={StaffStackScreen} 
-                options={{tabBarLabel: 'Nhân sự',
-                  tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons 
-                        name="account-outline" 
-                        color={color} 
-                        size={26} />
-                  )}} />
-            <Tab.Screen 
-                name="More" 
-                component={MoreStackScreen} 
-                options={{tabBarLabel: 'Mở rộng',
-                  tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons 
-                        name="layers-outline" 
-                        color={color} 
-                        size={26} />
-                  )}} />
-          </Tab.Navigator> */}
-      </NavigationContainer>
-  )
+const App = () => {
+  // const [isLoading, setIsLoading] = React.useState(true);
+  // const [userToken, setUserToken] = React.useState(null); 
+
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
+
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
+
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+
+  const authContext = React.useMemo(() => ({
+    signIn: async(foundUser) => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+      const userToken = String(foundUser[0].userToken);
+      const userName = foundUser[0].username;
+      
+      try {
+        await AsyncStorage.setItem('userToken', userToken);
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'LOGIN', id: userName, token: userToken });
+    },
+    signOut: async() => {
+      // setUserToken(null);
+      // setIsLoading(false);
+      try {
+        await AsyncStorage.removeItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      dispatch({ type: 'LOGOUT' });
+    },
+    signUp: () => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+    }
+  }), []);
+
+  useEffect(() => {
+    setTimeout(async() => {
+      // setIsLoading(false);
+      let userToken;
+      userToken = null;
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'RETRIEVE_TOKEN', token: userToken });
+    }, 1000);
+  }, []);
+
+  if( loginState.isLoading ) {
+    return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+  return (
+    <PaperProvider>
+    <AuthContext.Provider value={authContext}>
+    <NavigationContainer >
+      <RootStackScreen/>
+    </NavigationContainer>
+    </AuthContext.Provider>
+    </PaperProvider>
+  );
 }
 
-
+export default App;
