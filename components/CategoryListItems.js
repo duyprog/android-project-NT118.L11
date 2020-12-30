7f9e91b1a378bc4c7d7e3f9aea0f8bfda4b662cf
thatmiphilit <phihvm18@gmail.com>
2020-12-30 10:11:55 +0700

    update screen

--- components/CategoryListItems.js
+++ components/CategoryListItems.js
@@ -14,24 +14,19 @@ export default class CategoryListItems extends Component {
 
 const styles = StyleSheet.create({
     CategoryImage:{
-        width: 78, 
-        height: 78,
-        marginLeft: 10
+        width: 70,
+        height: 70
     },
     containter:{
-        alignItems: 'center',
-        padding: 14,
-        borderRadius: 6,
-        backgroundColor: '#FFF',
-        elevation: 5,
-        marginBottom: 14,
-        flexDirection: 'row'
+        alignSelf: 'center',
+        padding: 16,
+        marginLeft: 10,
+        marginRight: 10
     }, 
     title:{
-        textTransform: 'uppercase', 
-        fontWeight: '700',
         alignSelf: 'center',
-        fontSize: 18,
-        marginLeft: 20
+        fontSize: 16,
+        color: '#000',
+        marginTop: 5
     }
 })
